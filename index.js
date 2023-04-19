103e2b0448613c0137e8ca53b4cd031b727821fb
deniska69 <gostev.dy@yandex.ru>
2023-04-19 23:26:40 +0700

    upd config

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const start = async () => {
     try {
       app.listen(PORT, () => {
-        console.log(`\nBack-Сервер '${NAME_PROJECT}' запущен [`, new Date().toLocaleString("ru-RU", { timeZone: "Europe/Moscow" }), "(МСК)]. Порт:", PORT);
+        console.log(`\nBack-Сервер '${NAME_PROJECT}' запущен [`, new Date().toLocaleString("ru-RU", { timeZone: "Europe/Moscow" }), "(МСК) ]. Порт:", PORT);
   
         var twirlTimer = (function () {
             var P = ["\\", "|", "/", "-"];
