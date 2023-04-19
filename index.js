4d687f37bdd9366464dda64b2a4bd9ea8db920a1
deniska69 <gostev.dy@yandex.ru>
2023-04-20 02:49:55 +0700

    add cors and configured socket io connections

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 require("dotenv").config();
-const express = require('express');
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 5000;
 const NAME_PROJECT = process.env.NAME_PROJECT;
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({ origin: "*" }));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  }
+})
 
 const start = async () => {
-    try {
-      app.listen(PORT, () => {
-        console.log(`\nBack-Сервер '${NAME_PROJECT}' запущен [`, new Date().toLocaleString("ru-RU", { timeZone: "Europe/Moscow" }), "(МСК) ]. Порт:", PORT);
-  
-        var twirlTimer = (function () {
-            var P = ["\\", "|", "/", "-"];
-            var x = 0;
-            return setInterval(function () {
-                process.stdout.write("\r" + P[x++]);
-                x &= 3;
-            }, 250);
-        })();
+  try {
+    //TODO move to socket module
+    io.on("connection", (socket) => {
+      console.log('\nsocket connect:', socket?.id);
+
+      io.on("disconnect", () => {
+        console.error("\nsocket disconnect");
       });
-    } catch (e) {
-        console.log(e);
-    }
+    });
+
+    server.listen(PORT, () => {
+      console.log(`\nBackend-Server '${NAME_PROJECT}' started [`, new Date().toLocaleString("ru-RU", { timeZone: "Europe/Moscow" }), "(MSK) ]. Port:", PORT);
+
+      var twirlTimer = (function () {
+          var P = ["\\", "|", "/", "-"];
+          var x = 0;
+          return setInterval(function () {
+              process.stdout.write("\r" + P[x++]);
+              x &= 3;
+          }, 250);
+      })();
+    });
+  } catch (e) {
+      console.error(e);
+  }
 };
   
 start();
